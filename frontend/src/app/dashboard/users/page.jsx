ea30970fc733b0955b271cb5e614383f7ffeb2d3
surajgoswami11 <oodinsonthor11@gmail.com>
2025-08-26 08:10:50 +0530

    new changes

--- frontend/src/app/dashboard/users/page.jsx
+++
@@ -1,9 +0,0 @@
-import { Typography } from "@mui/material";
-
-export default function User() {
-    return (
-        <>
-            <Typography>This is user list page</Typography>
-        </>
-    )
-}
