cf6e3256ea9c40a8921c093bee61eb531000bfa5
surajgoswami03 <surajgoswami7866@gmail.com>
2025-08-29 09:58:53 +0530

    my code

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT;
 app.use(
   cors({
     // origin: process.env.URL,
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3002",
     credentials: true,
   })
 );
